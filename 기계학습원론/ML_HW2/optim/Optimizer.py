348f8db3c5cc79b5fa091719fa01488d052f0766
JuyeoungJun <49807087+JuyeoungJun@users.noreply.github.com>
2020-08-28 18:26:19 +0900

    Create Optimizer.py

---
+++ 기계학습원론/ML_HW2/optim/Optimizer.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class SGD:
+    # ========================= EDIT HERE= ========================
+    def __init__(self):
+        pass
+
+    def update(self, w, grad, lr):
+        return w - lr * grad
+        
+    # =============================================================
